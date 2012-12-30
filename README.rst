7bc419d416fdb4e6e03ee3c657831bfd493c2111
Corey Goldberg <cgoldberg@gmail.com>
2012-12-30 14:54:35 -0500

    more initial development

--- README.rst
+++ README.rst
@@ -8,7 +8,7 @@ sauceclient
 
 Work in Progress...
 
- - @cgoldberg (12/29/2012)
+ - @cgoldberg (12/30/2012)
  
 ----
 
@@ -16,36 +16,41 @@ Work in Progress...
 about sauceclient
 -----------------
 
-`sauceclient` is *not* for running your tests on Sauce Labs service.  That is done via Selenium WebDriver.
-
 `sauceclient` is for interacting with Sauce Labs to retrieve and update information about resources such as:
 
+ * Information
+ * Jobs
  * Provisioning
  * Usage
- * Jobs
- * Information
 
-Sauce Labs REST API documentation:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+`sauceclient` is *not* for running your tests on Sauce Labs service.  That is done via Selenium WebDriver.
+
+---------------------------------
+Sauce Labs REST API documentation
+---------------------------------
 
 * http://saucelabs.com/docs/rest
 
-----------------------------------
-a simple example using sauceclient
-----------------------------------
+-------------------------
+example using sauceclient
+-------------------------
 
-* using sauceclient::
+* accessing publicly::
 
     import sauceclient
+    
+    sc = sauceclient.SauceClient()
+    status = sc.information.get_status()
+    
+* with authorization::
 
+    import sauceclient
+    
     sc = sauceclient.SauceClient(
         'sauce-username',
         'sauce-access-key',
     )
-            
-    job_ids = sc.jobs.list_job_ids()
-    a_job = job_ids[0]
-    attributes = sc.jobs.get_job_attributes(a_job)
+    job_ids = sc.jobs.get_job_ids()
 
 ----------------------------------
 running the sauceclient unit tests
@@ -54,14 +59,19 @@ running the sauceclient unit tests
 * clone the repo::
 
     $ git clone git@github.com:cgoldberg/sauceclient.git
+    $ cd sauceclient
 
-* edit `./sauceclient/sauceclient/test_sauceclient.py`, and change the 
+* tests are located in::
+
+    sauceclient/test_sauceclient.py
+    
+* edit `test_sauceclient.py`, and change the 
   test parameters to match your Sauce Labs account info::
 
     SAUCE_USERNAME = 'your-username-string'
     SAUCE_ACCESS_KEY = 'your-access-key-string'
     TEST_JOB_ID = 'a-valid-test-job-id'
 
-* run tests using standard `unittest` discovery::
+* run tests by executing `test_sauceclient.py`, or using `unittest` discovery::
 
-    $ python unittest -m discover
+    $ python -m unittest discover
--- sauceclient/test_sauceclient.py
+++ sauceclient/test_sauceclient.py
@@ -15,7 +15,7 @@
 
 import sauceclient
 
-import json
+import random
 import unittest
 
 
@@ -26,12 +26,19 @@ TEST_JOB_ID = ''  # any valid job
 
 
 class TestSauceClient(unittest.TestCase):
-    
+
     def setUp(self):
         self.sc = sauceclient.SauceClient(
             SAUCE_USERNAME,
             SAUCE_ACCESS_KEY,
         )
+
+    def test_has_instances(self):
+        self.assertIsInstance(self.sc.information, sauceclient.Information)
+        self.assertIsInstance(self.sc.jobs, sauceclient.Jobs)
+        self.assertIsInstance(self.sc.provisioning, sauceclient.Provisioning)
+        self.assertIsInstance(self.sc.usage, sauceclient.Usage)
+
     def test_headers(self):
         headers = self.sc.headers
         self.assertIsInstance(headers, dict)
@@ -39,13 +46,11 @@ class TestSauceClient(unittest.TestCase):
         self.assertIn('Content-Type', headers)
         self.assertIn('Basic', headers['Authorization'])
         self.assertEqual('application/json', headers['Content-Type'])
-        
+
     def test_request_get(self):
-        url = '/rest/v1/users/%s' % self.sc.sauce_username
+        url = 'rest/v1/info/status'
         json_data = self.sc.request('GET', url)
         self.assertIsInstance(json_data, str)
-        attributes = json.loads(json_data)
-        self.assertIsInstance(attributes, dict)
 
 
 class TestJobs(unittest.TestCase):
@@ -55,38 +60,52 @@ class TestJobs(unittest.TestCase):
             SAUCE_USERNAME,
             SAUCE_ACCESS_KEY,
         )
-        self.jobs = sauceclient.Jobs(self.sc)
 
-    def test_list_job_ids(self):
-        job_ids = self.jobs.list_job_ids()
+    def test_get_job_ids(self):
+        job_ids = self.sc.jobs.get_job_ids()
         self.assertIsInstance(job_ids, list)
-        job_id = job_ids[0]
+        job_id = random.choice(job_ids)
         self.assertIsInstance(job_id, unicode)
         self.assertTrue(job_id.isalnum())
 
-    def test_list_jobs(self):
-        jobs = self.jobs.list_jobs()
+    def test_get_jobs(self):
+        jobs = self.sc.jobs.get_jobs()
         self.assertIsInstance(jobs, list)
-        job = jobs[0]
+        job = random.choice(jobs)
         self.assertIn('id', job)
         self.assertIsInstance(job['id'], unicode)
-        self.assertEqual(job['owner'], SAUCE_USERNAME)
+        self.assertEqual(job['owner'], self.sc.sauce_username)
 
     def test_get_job_attributes(self):
-        job_attributes = self.jobs.get_job_attributes(TEST_JOB_ID)
+        job_attributes = self.sc.jobs.get_job_attributes(TEST_JOB_ID)
         self.assertIsInstance(job_attributes, dict)
         self.assertIn('id', job_attributes)
+        self.assertIn('status', job_attributes)
+        self.assertIn('commands_not_successful', job_attributes)
+        self.assertIn('name', job_attributes)
+        self.assertIn('video_url', job_attributes)
+        self.assertIn('tags', job_attributes)
+        self.assertIn('start_time', job_attributes)
+        self.assertIn('log_url', job_attributes)
+        self.assertIn('creation_time', job_attributes)
+        self.assertIn('custom-data', job_attributes)
+        self.assertIn('browser_version', job_attributes)
+        self.assertIn('end_time', job_attributes)
+        self.assertIn('passed', job_attributes)
+        self.assertIn('owner', job_attributes)
+        self.assertIn('browser', job_attributes)
+        self.assertIn('os', job_attributes)
+        self.assertIn('public', job_attributes)
+        self.assertIn('breakpointed', job_attributes)
+        self.assertIn('build', job_attributes)
         self.assertEqual(job_attributes['id'], TEST_JOB_ID)
+        self.assertIn(job_attributes['owner'], self.sc.sauce_username)
 
     def test_update_job(self):
-        job_attributes = self.jobs.update_job(TEST_JOB_ID)
+        job_attributes = self.sc.jobs.update_job(TEST_JOB_ID)
         self.assertIsInstance(job_attributes, dict)
         self.assertIn('id', job_attributes)
         self.assertEqual(job_attributes['id'], TEST_JOB_ID)
-
-    def tearDown(self):
-        SAUCE_USERNAME = None
-        SAUCE_ACCESS_KEY = None
 
 
 class TestProvisioning(unittest.TestCase):
@@ -96,22 +115,106 @@ class TestProvisioning(unittest.TestCase):
             SAUCE_USERNAME,
             SAUCE_ACCESS_KEY,
         )
-        self.p = sauceclient.Provisioning(self.sc)
 
     def test_get_account_details(self):
-        account_details = self.p.get_account_details()
+        account_details = self.sc.provisioning.get_account_details()
         self.assertIsInstance(account_details, dict)
         self.assertIn('id', account_details)
         self.assertIn('minutes', account_details)
         self.assertIn('access_key', account_details)
         self.assertIn('subscribed', account_details)
-        self.assertEqual(account_details['id'], SAUCE_USERNAME)
+        self.assertEqual(account_details['id'], self.sc.sauce_username)
 
     def test_get_account_limits(self):
-        account_limits = self.p.get_account_limits()
+        account_limits = self.sc.provisioning.get_account_limits()
         self.assertIsInstance(account_limits, dict)
         self.assertIn('concurrency', account_limits)
-        #self.assertEqual(account_details['id'], SAUCE_USERNAME)
+        self.assertTrue(account_limits['concurrency'] > 0)
+
+
+class TestInformation(unittest.TestCase):
+
+    def setUp(self):
+        self.sc = sauceclient.SauceClient()
+
+    def test_get_status(self):
+        status = self.sc.information.get_status()
+        self.assertIsInstance(status, dict)
+        self.assertIn('service_operational', status)
+        self.assertIn('status_message', status)
+        self.assertIn('wait_time', status)
+        self.assertIsInstance(status['status_message'], unicode)
+        self.assertTrue(status['service_operational'])
+
+    def test_get_status_with_auth(self):
+        sc = sauceclient.SauceClient(
+            SAUCE_USERNAME,
+            SAUCE_ACCESS_KEY,
+        )
+        status = sc.information.get_status()
+        self.assertIsInstance(status, dict)
+        self.assertIn('service_operational', status)
+        self.assertIn('status_message', status)
+        self.assertIn('wait_time', status)
+        self.assertTrue(status['service_operational'])
+
+    def test_get_browswers(self):
+        browsers = self.sc.information.get_browsers()
+        self.assertIsInstance(browsers, list)
+        self.assertTrue(len(browsers) > 0)
+        browser = random.choice(browsers)
+        self.assertIn('automation_backend', browser)
+        self.assertIn('long_name', browser)
+        self.assertIn('long_version', browser)
+        self.assertIn('os', browser)
+        self.assertIn('preferred_version', browser)
+        self.assertIn('selenium_name', browser)
+        self.assertIn('short_version', browser)
+        self.assertIsInstance(browser['selenium_name'], unicode)
+
+    def test_get_count(self):
+        count = self.sc.information.get_count()
+        self.assertIsInstance(count, int)
+        self.assertTrue(count > 20000000)
+
+
+class TestUsage(unittest.TestCase):
+
+    def setUp(self):
+        self.sc = sauceclient.SauceClient(
+            SAUCE_USERNAME,
+            SAUCE_ACCESS_KEY,
+        )
+
+    def test_get_current_activity(self):
+        activity = self.sc.usage.get_current_activity()
+        self.assertIsInstance(activity, dict)
+
+        self.assertIn('subaccounts', activity)
+        self.assertIn(SAUCE_USERNAME, activity['subaccounts'])
+
+        subaccount_activity = activity['subaccounts'][self.sc.sauce_username]
+        self.assertIn('all', subaccount_activity)
+        self.assertIsInstance(subaccount_activity['all'], int)
+        self.assertIn('in progress', subaccount_activity)
+        self.assertIsInstance(subaccount_activity['in progress'], int)
+        self.assertIn('queued', subaccount_activity)
+        self.assertIsInstance(subaccount_activity['queued'], int)
+
+        self.assertIn('totals', activity)
+        self.assertIn('all', activity['totals'])
+        self.assertIsInstance(activity['totals']['all'], int)
+        self.assertIn('in progress', activity['totals'])
+        self.assertIsInstance(activity['totals']['in progress'], int)
+        self.assertIn('queued', activity['totals'])
+        self.assertIsInstance(activity['totals']['queued'], int)
+
+    def test_get_historical_usage(self):
+        historical_usage = self.sc.usage.get_historical_usage()
+        self.assertIn('usage', historical_usage)
+        self.assertIn('username', historical_usage)
+        self.assertEqual(historical_usage['username'], self.sc.sauce_username)
+        self.assertIsInstance(historical_usage['usage'], list)
 
 
 if __name__ == '__main__':
