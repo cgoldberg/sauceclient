3c12a663aeadf06d21a35edffd27ca887d896392
Drew Engelson <drew@engelson.net>
2016-11-03 00:16:19 -0400

    Added docstring for auth token method.

--- sauceclient.py
+++ sauceclient.py
@@ -335,6 +335,10 @@ class Jobs(object):
         return self.client.request(method, url)
 
     def get_auth_token(self, job_id, date_range=None):
+        """Get an auth token to access protected job resources.
+        
+        https://wiki.saucelabs.com/display/DOCS/Building+Links+to+Test+Results
+        """
         key = '{}:{}'.format(self.client.sauce_username, self.client.sauce_access_key)
         if date_range:
             key = '{}:{}'.format(key, date_range)
