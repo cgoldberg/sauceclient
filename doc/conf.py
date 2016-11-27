0558f632f49bd73735643098751be02810bc03c1
Corey Goldberg <cgoldberg@gmail.com>
2016-11-27 08:51:31 -0500

    update sphinx stuff for docs

--- doc/conf.py
+++ doc/conf.py
@@ -11,6 +11,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -43,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'sauceclient'
-copyright = u'2013-2015, Corey Goldberg'
+copyright = u'2013-2016, Corey Goldberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -94,6 +95,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
